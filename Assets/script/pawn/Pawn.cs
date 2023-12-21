87c0d20a8ad37f49be8ed57b01cf81514cb03108
Aethernum <martinsko@sfr.fr>
2023-12-21 18:29:06 +0100

    add projection effect

--- Assets/script/pawn/Pawn.cs
+++ Assets/script/pawn/Pawn.cs
@@ -12,7 +12,9 @@ public enum PawnType
     }
 public class Pawn : MonoBehaviour
 {
-
+    private bool isMouseDragging = false;
+    private Vector3 clickPosition;
+    private Rigidbody rb;
     [SerializeField] private string heroesName;
     [SerializeField] private float heroesSpeed;
     
@@ -20,12 +22,61 @@ public class Pawn : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            TryStartDrag();
+            
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            if(isMouseDragging){
+                OnMouseUp();
+            }
+            
+        }
+    }
+    void TryStartDrag()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit) && hit.collider.gameObject == gameObject)
+        {
+            // Le clic de la souris a commencé sur le pawn
+            OnMouseDown();
+            isMouseDragging = true;
+        }
+    }
+    private void OnMouseUp()
+    {
+         Vector3 currentMousePosition = Input.mousePosition;
+        currentMousePosition.z = Camera.main.WorldToScreenPoint(transform.position).z;
+
+        // Convertir la position actuelle de la souris en position dans l'espace du monde
+        currentMousePosition = Camera.main.ScreenToWorldPoint(currentMousePosition);
+
+        // Calculer la direction et la distance entre le clic initial et la position actuelle de la souris
+        Vector3 dragDirection = currentMousePosition - clickPosition;
+
+        // Appliquer une force proportionnelle à la distance pour simuler la projection
+        rb.AddForce(-dragDirection * heroesSpeed, ForceMode.Impulse);
+        isMouseDragging = false;
+    }
+
+    
+    private void OnMouseDown()
+    {
+       clickPosition = Input.mousePosition;
+        clickPosition.z = Camera.main.WorldToScreenPoint(transform.position).z;
+
+        // Convertir la position du clic en position dans l'espace du monde
+        clickPosition = Camera.main.ScreenToWorldPoint(clickPosition);
     }
 }
